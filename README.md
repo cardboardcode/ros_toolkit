ccc332049700c63a10f655a77de161f6e4239f54
Bey Hao Yun <beyhy94@gmail.com>
2022-08-26 23:26:01 +0800

    :heavy_plus_sign: Added unverified experimental bash script to automate installation of ROS2 Foxy.

---
+++ README.md
@@ -0,0 +1,9 @@
+## **What Is This?**
+
+This repository contains `.bash` scripts that automate certain tedious processes concerning development in Robot Operating System ( **ROS** ).
+
+## **How To Use**
+
+```bash
+wget <url_to_script_in_specific_folder> 
+```
---
+++ install/foxy/install_debian.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Set Locale to UTF-8
+sudo apt-get update 
+sudo apt-get install -y locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+locale
+
+# Set up Sources
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository universe
+sudo apt-get update
+sudo apt-get install -y curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# Install ROS2 packages
+sudo apt-get update
+sudo apt-get install -y ros-foxy-desktop
+
+# Install colcon build tools
+sudo apt-get install -y python3-colcon-common-extensions
+
+
